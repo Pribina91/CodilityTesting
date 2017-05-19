4dcd5d836ed371d5458fbd47e5dc96603d2ae518
Andrej Stajer <adostajo@gmail.com>
2017-05-19 04:58:25 +0200

    First commit

---
+++ CodilityLessons/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().solution(16);
+            new Program().solution(8);
+        }
+
+        public int solution(int N)
+        {
+            Console.WriteLine(Convert.ToString(N, 2));
+
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var maxBinaryGap = 0;
+            var currentGap = 0;
+            foreach (var bit in Convert.ToString(N, 2))
+            {
+                if (bit == '0')
+                {
+                    currentGap++;
+                }
+                else
+                {
+                    if (currentGap > maxBinaryGap)
+                    {
+                        maxBinaryGap = currentGap;
+                    }
+                    currentGap = 0;
+                }
+            }
+
+            return maxBinaryGap;
+        }
+    }
+}
---
+++ Lesson2A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Program().solution(new[] {1, 1, 2});
+        }
+
+        public int solution(int[] A)
+        {
+            var nonPairedValues = new HashSet<int>();
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            foreach (var itemValue in A)
+            {
+                if (nonPairedValues.Contains(itemValue))
+                {
+                    nonPairedValues.Remove(itemValue);
+                    continue;
+                }
+                else
+                {
+                    nonPairedValues.Add(itemValue);
+                }
+            }
+
+            if (!nonPairedValues.Any())
+            {
+                throw new Exception();
+            }
+
+            return nonPairedValues.First();
+        }
+    }
+}
