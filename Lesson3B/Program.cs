bd7b304932d7e77b7c161b5e7739c3f0ad323055
Andrej Stajer <adostajo@gmail.com>
2017-05-23 04:51:17 +0200

    -lesson 3b -lesson 3c init

---
+++ Lesson3B/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().solution(10, 80, 100));
+        }
+
+        public int solution(int X, int Y, int D)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var result = Math.Ceiling((Y - X) / (double)D);
+            return Convert.ToInt32(result);
+        }
+    }
+}
