95e6309da45513b5afe6e79297b902aea5697570
Andrej Stajer <adostajo@gmail.com>
2017-06-01 20:57:50 +0200

    stop watch implementation

---
+++ Stopwatch/LessonStopwatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    public class LessonStopwatch
+    {
+        private System.Diagnostics.Stopwatch _stopwatch;
+
+        public LessonStopwatch() : base()
+        {
+            _stopwatch = new System.Diagnostics.Stopwatch();
+        }
+
+        public void Execute(Action action)
+        {
+            _stopwatch.Restart();
+            action();
+            _stopwatch.Stop();
+            Console.WriteLine(_stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
