7ffef304f3d31b4fb57873fa60fc9604338f2d0e
Andrej Stajer <adostajo@gmail.com>
2017-08-03 13:28:52 +0900

    PassingCars

---
+++ Lesson5A/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().solution(new int[] { 0,1,0,1,1 }));
+            Console.WriteLine(new Program().solution(new int[] { 0,1,0,1,1,1 }));
+            Console.WriteLine(new Program().solution(new int[] { 0,1,0,1,1,1,0 }));
+            Console.WriteLine(new Program().solution(new int[] { 1,0,1,0,1,1,1,0 }));
+            Console.WriteLine(new Program().solution(Enumerable.Repeat(0, 50000).Concat(Enumerable.Repeat(1, 50000)).ToArray()));
+
+        }
+
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            //prefix sums
+            if (!A.Any())
+            {
+                throw new ArgumentException("Parameter empty", nameof(A));
+            }
+
+            int pairs = 0;
+            int prefixSum = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0) //east
+                {
+                    prefixSum++;
+                }
+                else // west based on precondition that A contains only 0 and 1
+                //else if(A[i] == 1) // west
+                {
+                    pairs += prefixSum;
+                }
+
+                if (pairs > 1000000000)
+                {
+                    return -1;
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Tst
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FirstNames = new List<string>() {"Bob", "Sondra", "Avery", "Von", "Randle", "Gwen", "Paisley"};
+            var LastNames = new List<string>() {"Anderson", "Carlson", "Vickers", "Black", "Schultz", "Marigold", "Johnson"};
+            var Birthdates = new List<DateTime>()
+            {
+                Convert.ToDateTime("11/12/1980"),
+                Convert.ToDateTime("09/16/1978"),
+                Convert.ToDateTime("05/18/1985"),
+                Convert.ToDateTime("10/29/1980"),
+                Convert.ToDateTime("01/19/1989"),
+                Convert.ToDateTime("01/14/1972"),
+                Convert.ToDateTime("02/20/1981")
+            };
+
+            var resultList = new List<Tst>();
+            for (int i = 0; i < FirstNames.Count; i++)
+            {
+                resultList.Add(
+                    new Tst()
+                    {
+                        Id = i,
+                        Name = FirstNames[i]
+                    }
+                );
+            }
+        }
+    }
+}
